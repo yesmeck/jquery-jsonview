74e344016be8e5291f0728b689d3092cd8a38a5e
Meck <yesmeck@gmail.com>
2012-12-24 02:38:05 +0800

    change id "json" to "jsonview"

--- jquery.jsonview.js
+++ jquery.jsonview.js
@@ -87,7 +87,7 @@
     // Convert a whole JSON object into a formatted HTML document.
     jsonToHTML: function(json) {
       var output = '';
-      output += '<div id="json">';
+      output += '<div id="jsonview">';
       output += this.valueToHTML(json);
       output += '</div>';
       return output;
@@ -99,7 +99,7 @@
       // output += '<h1>' + this.stringbundle.GetStringFromName('docContents') + ':</h1>';
       var output = '<div id="error">Error parsing JSON: '+error.message+'</div>';
       output += '<h1>'+error.stack+':</h1>';
-      output += '<div id="json">' + this.htmlEncode(data) + '</div>';
+      output += '<div id="jsonview">' + this.htmlEncode(data) + '</div>';
       return this.toHTML(output, uri + ' - Error');
     }
   };
